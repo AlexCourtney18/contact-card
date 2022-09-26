c2bdcd1f947c26eda34f7693130496cd8e59406f
Alex <lex.courtney18@gmail.com>
2022-09-26 14:23:47 -0500

    19.3.4

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,11 +9,14 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-import { initdb } from './database';
+import { initdb, getDb, postDb } from './database';
 
 
 window.addEventListener('load', function () {
     initdb();
+    getDb();
+    postDb("Packerfan", "packerfan@email.com", 4148121212, "Lambeau");
+    getDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;

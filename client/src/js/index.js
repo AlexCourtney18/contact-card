a485d94769ad01aa556ba352532975fb64130d80
Alex <lex.courtney18@gmail.com>
2022-09-26 15:53:34 -0500

    fix minor bug causing console error

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,16 +1,15 @@
-import "./form";
+import { initdb, postDb, deleteDb, editDb } from './database';
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
 
 import "../css/index.css";
-import { Tooltip, Toast, Popver } from 'bootstrap';
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-
-import { initdb, getDb, postDb, deleteDb, editDb } from './database';
-import { fetchCards } from './cards';
-import { toggleForm, clearForm } from './form';
 
 
 window.addEventListener('load', function () {

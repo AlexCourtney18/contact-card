5df0324fecd546ceffc23911e9c2f80a6bf0acf7
Alex <lex.courtney18@gmail.com>
2022-09-27 12:00:20 -0500

    19.5.4

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -23,12 +23,28 @@ window.addEventListener('load', function () {
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
+const installBtn = document.getElementById("installBtn");
 let submitBtnToUpdate = false;
 let profileId;
 
 newContactButton.addEventListener('click', event => {
     toggleForm()
 })
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+    
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        installBtn.setAttribute('disabled', true);
+        installBtn.textContent = 'Installed!';
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('Well done!', 'appinstalled', event);
+});
 
 form.addEventListener('submit', event => {
     // Handle data
